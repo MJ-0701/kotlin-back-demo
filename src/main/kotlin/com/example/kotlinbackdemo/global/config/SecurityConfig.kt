920367691bf8899b59469604cab3cd1f2e357843
MJ-0701 <jack2718@naver.com>
2023-04-08 04:16:13 +0900

    feat(security) : add jwt config

--- src/main/kotlin/com/example/kotlinbackdemo/global/config/SecurityConfig.kt
+++ src/main/kotlin/com/example/kotlinbackdemo/global/config/SecurityConfig.kt
@@ -1,5 +1,7 @@
 package com.example.kotlinbackdemo.global.config
 
+import com.example.kotlinbackdemo.global.config.jwt.JwtAuthenticationFilter
+import com.example.kotlinbackdemo.global.config.jwt.JwtTokenProvider
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.core.annotation.Order
@@ -13,6 +15,7 @@ import org.springframework.security.config.annotation.web.configurers.*
 import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer.AuthorizationManagerRequestMatcherRegistry
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 import org.springframework.web.cors.CorsConfiguration
 import org.springframework.web.cors.CorsConfigurationSource
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource
@@ -20,7 +23,9 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 
 @Configuration
 @EnableWebSecurity
-class SecurityConfig() {
+class SecurityConfig(
+    private val jwtTokenProvider: JwtTokenProvider
+) {
 
     @Bean
     @Throws(Exception::class)
@@ -48,6 +53,9 @@ class SecurityConfig() {
         return http
             .httpBasic().disable() // rest api만 고려 기본설정 해제
             .csrf().disable() // csrf 보안토큰 disable
+            .addFilterBefore(
+                JwtAuthenticationFilter(jwtTokenProvider),
+                UsernamePasswordAuthenticationFilter::class.java)
             .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 토큰 기반 인증이므로 세션 사용안함
             .and()
             .authorizeRequests() // 요총에 대한 권한 체크
